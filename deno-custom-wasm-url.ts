a1ee83281d91bd2336864341969363101aa9bad3
Sergei Blinov <blinovsv@gmail.com>
2022-12-11 19:14:02 +0100

    add url workaround

---
+++ deno-custom-wasm-url.ts
@@ -0,0 +1,21 @@
+import { TonClient } from "npm:@eversdk/core";
+import { libWeb, libWebSetup } from "./node_modules/@eversdk/lib-web/index.js";
+
+// require npm i @eversdk/lib-web
+libWebSetup({
+  disableSeparateWorker: true,
+  binaryURL: new URL(
+    "./node_modules/@eversdk/lib-web/eversdk.wasm",
+    import.meta.url,
+  ),
+});
+
+TonClient.useBinaryLibrary(libWeb);
+// ------------------------^^^^^^
+// here it will complain in linter but it's OK
+
+const client = new TonClient();
+const { phrase } = await client.crypto.mnemonic_from_random({});
+console.log(phrase);
+console.log(await client.crypto.mnemonic_derive_sign_keys({ phrase }));
+client.close();
